600c47adcc42d7a50129f8cf8b1e708a191e5096
amiralirasoli <amiralirasoli2003@gmail.com>
2026-04-15 22:55:08 -0700

    completed task 2 included type casting and  String vaiable

---
+++ Day 2 task/Main1.java
@@ -0,0 +1,22 @@
+
+package main.pkg1;
+
+
+public class Main1 {
+
+   
+    public static void main(String[] args) {
+        
+         String name = "amir ali";
+        int age= 21;
+        double Gpa = 3.44;
+        // casting double to int 
+        int finalgrade = (int)Gpa;
+        System.out.print(" name: "+ name +  " age: "+ age +  " finalgrade(Rounded) " + finalgrade );
+
+
+    }
+    
+}
+
+        
