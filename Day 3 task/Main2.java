058668f7937efc530129f62fa040107a1c51714c
amiralirasoli <amiralirasoli2003@gmail.com>
2026-04-15 23:39:37 -0700

    finding the largest number between two integer by using Math class

---
+++ Day 3 task/Main2.java
@@ -0,0 +1,15 @@
+
+package main.pkg2;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+       int number1 = 22;
+       int number2 = 34;
+       System.out.println(Math.max(number1, number2));
+       
+       
+       
+    }
+    
+}
