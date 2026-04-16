c605f43008cd1156bcdc8ff4972631d19a12fc79
mahdi <hatamimahdi32@gmail.com>
2026-04-16 10:59:32 -0700

    Day 4 task

---
+++ Day 4 task/Main.java
@@ -0,0 +1,17 @@
+//Day 4: Java Booleans and Conditionals
+//Task: Write a program that checks if a user has "Superuser" status based on an ID number.
+//1. Create an integer variable userID.
+// 2. Create a boolean variable isSuperuser.
+// 3. Use an if/else statement:
+// - If userID is exactly 777, set isSuperuser to true.
+// - Otherwise, set isSuperuser to false.
+// 4. Print the final status.
+// Starter code:
+public class Main {
+public static void main(String[] args) {
+int userID = 777;
+boolean isSuperuser;
+// TODO: Create if/else logic here
+// TODO: Print "Is Superuser: " + isSuperuser
+}
+}
