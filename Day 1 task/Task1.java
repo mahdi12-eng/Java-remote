4f42483ad14ea7ecd5f98b36962e94fc9bee5ca4
Ali <aliparsajaghori@gmail.com>
2026-04-19 09:09:32 +0430

    finding sum of two numbers.

---
+++ Day 1 task/Task1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mavenproject1.task1;
+
+public class Task1 {
+ public static void main(String[] args) {
+     int a = 5;
+     int b = 10;
+     int c = a + b;
+    
+        System.out.println("The sum of these two numbers are: " + (a+b));
+        System.out.println ("the sum of these two numbers equal to: " + c);
+    }
+}
