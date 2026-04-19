a03cfbc65bf8aad61674d23910ea6451e4deb237
Ali <aliparsajaghori@gmail.com>
2026-04-19 09:38:35 +0430

    the second task: information of a student.

---
+++ Day 2 task/Task2.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mavenproject1.task2;
+
+public class Task2 {
+ public static void main(String[] args) {
+    String name = "Ali";
+    int age = 20;
+    double gpa = 4.56d;
+    int finalgrade = (int)gpa;
+    
+        System.out.println("Student " + name + " with the age of " + age + " and exact GPA of " + gpa + " ,and final grade of" + finalgrade + " is our student.");
+    }
+}
