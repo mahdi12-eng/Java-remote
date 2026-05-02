1179f821d38ef32ea1b19ad8352e7907a21a5c96
Ali <aliparsajaghori@gmail.com>
2026-05-02 20:31:21 +0430

    Task of string and math

---
+++ Day 3 task/Task 3.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.mavenproject1.task3;
+
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        // The first task
+        int x = 4;
+        int y = 6;
+        System.out.println("the sum of these two numbers is: " + x+y);
+        // The second task
+        String name = "MOhammad";
+        System.out.println("the length of this name is: " + name.length());
+        // the third task
+        int a = 23;
+        int b =76;
+        System.out.println("the larger number is: " + Math.max(a, b));
+        
+        
+        
+    }
+}
