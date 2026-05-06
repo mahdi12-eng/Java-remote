84be3c086e5dbc06d66ad5340a37fa0ca1540c2a
Ali <aliparsajaghori@gmail.com>
2026-05-06 15:07:44 +0430

    the simple if...else

---
+++ Day 4 task/Task4.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.mavenproject1.task4;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        int idnum = 77;
+       
+        if (idnum == 777)
+            System.out.println( "is superuser: True ");
+        else
+            System.out.println("is superuser: False");
+                
+        
+    }
+}
